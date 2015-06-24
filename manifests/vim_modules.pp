0543bb45d61150a18179c9544e29d7cd72f06993
Jerald Sheets <questy@gmail.com>
2015-06-23 22:21:08 -0500

    completing vim modules additions

--- manifests/vim_modules.pp
+++ manifests/vim_modules.pp
@@ -5,7 +5,7 @@ class puppetdev::vim_modules (
   $group = $::puppetdev::params::group,
 ){
   # Install Powerline
-  vcsrepo {"/home/${user}/.vim/powerline":
+  vcsrepo {"/home/${user}/.vim/bundle/powerline":
     ensure   => 'present',
     provider => 'git',
     source   => 'git://github.com/powerline/powerline.git',
@@ -13,12 +13,66 @@ class puppetdev::vim_modules (
   }
 
   # Install Syntastic
-  vcsrepo {"/home/${user}/.vim/syntastic":
+  vcsrepo {"/home/${user}/.vim/bundle/syntastic":
     ensure   => 'present',
     provider => 'git',
     source   => 'https://github.com/scrooloose/syntastic.git',
     require  => Package['vim-enhanced'],
   }
 
+  # Install  Tabular
+  vcsrepo {"/home/${user}/.vim/bundle/tabular":
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'git://github.com/godlygeek/tabular.git',
+    require  => Package['vim-enhanced'],
+  }
 
+  # Install airline
+  vcsrepo {"/home/${user}/.vim/bundle/vim-airline":
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'https://github.com/bling/vim-airline.git',
+    require  => Package['vim-enhanced'],
+  }
+
+  # Install fugitive
+  vcsrepo {"/home/${user}/.vim/bundle/vim-fugitive":
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'https://github.com/tpope/vim-fugitive.git',
+    require  => Package['vim-enhanced'],
+  }
+
+  # Install Git Gutter
+  vcsrepo {"/home/${user}/.vim/bundle/vim-gitgutter":
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'https://github.com/airblade/vim-gitgutter.git',
+    require  => Package['vim-enhanced'],
+  }
+
+  # Install Indent Guides
+  vcsrepo {"/home/${user}/.vim/bundle/vim-indent-guides":
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'https://github.com/nathanaelkane/vim-indent-guides.git',
+    require  => Package['vim-enhanced'],
+  }
+
+  # Install Vim Puppet
+  vcsrepo {"/home/${user}/.vim/bundle/vim-puppet":
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'git://github.com/rodjek/vim-puppet.git',
+    require  => Package['vim-hanced'],
+  }
+
+  # Install Vim Trailing Whitespace
+  vcsrepo {"/home/${user}/.vim/bundle/vim-trailing-whitespace":
+    ensure   => 'present',
+    provider => 'git',
+    source   => 'https://github.com/bronson/vim-trailing-whitespace.git',
+    require  => Package['vim-enhanced'],
+  }
 }
