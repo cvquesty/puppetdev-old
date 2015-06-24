f68608bdf7d91d0db08b94ddd0d509175eab0d40
Jerald Sheets <questy@gmail.com>
2015-06-24 10:15:05 -0500

    syntax

--- manifests/vimrc.pp
+++ manifests/vimrc.pp
@@ -12,7 +12,7 @@ class puppetdev::vimrc {
 
 	# Make a place for the engineer to manage his own .vimrc settings
 	exec { 'vimrc.custom':
-		path    => '/usr/bin'
+		path    => '/usr/bin',
 		command => '/usr/bin/touch /home/vagrant/.vimrc.custom',
 		unless  => '/usr/bin/test -f /home/vagrant/.vimrc.custom',
 	}
