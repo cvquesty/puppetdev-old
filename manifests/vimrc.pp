81ae251b798f5692a0ebd2a05bbd6976c59ef17c
Jerald Sheets <questy@gmail.com>
2015-06-24 10:10:24 -0500

    made some adjustments for personal customizations

--- manifests/vimrc.pp
+++ manifests/vimrc.pp
@@ -9,4 +9,11 @@ class puppetdev::vimrc {
 		mode   => '0644',
 		source => 'puppet:///modules/puppetdev/vimrc',
 	}
+
+	# Make a place for the engineer to manage his own .vimrc settings
+	exec { 'vimrc.custom':
+		path    => '/usr/bin'
+		command => '/usr/bin/touch /home/vagrant/.vimrc.custom',
+		unless  => '/usr/bin/test -f /home/vagrant/.vimrc.custom',
+	}
 }
