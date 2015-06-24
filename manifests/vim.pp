936e03a4044b60896e947f3326a35f527600c84c
Jerald Sheets <questy@gmail.com>
2015-06-23 22:59:56 -0500

    remediating vim-minimal removal

--- manifests/vim.pp
+++ manifests/vim.pp
@@ -1,11 +1,6 @@
 # Main vim manifest for PuppetDev module
 #
 class puppetdev::vim {
-  # Remove vim-minimal
-  package {'vim-minimal':
-    ensure => 'absent',
-  }
-
   # Install vim-enhanced
   package {'vim-enhanced':
     ensure  => 'present',
