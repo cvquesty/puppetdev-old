352a0f877f49ac3690c9be1152dd2bcaa9e1b6e9
Jerald Sheets <questy@gmail.com>
2015-06-24 00:12:05 -0500

    adding proper git provider

--- manifests/vim.pp
+++ manifests/vim.pp
@@ -1,8 +1,14 @@
 # Main vim manifest for PuppetDev module
 #
 class puppetdev::vim {
+  # Make sure the git provider is available
+  package {'git':
+    ensure => 'present',
+  }
+
   # Install vim-enhanced
   package {'vim-enhanced':
     ensure  => 'present',
+    require => Package['git'],
   }
 }
