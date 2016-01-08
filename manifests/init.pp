406ba21132dc2f0fc7b56f6dad4ae1d3e2c154d8
Jerald Sheets <questy@gmail.com>
2016-01-07 20:11:16 -0500

    Cleaning up before starting refactor

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,46 +1,17 @@
 # == Class: puppetdev
 #
 # Full description of class puppetdev here.
-#
-# === Parameters
-#
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the function of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { 'puppetdev':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Jerald Sheets, unless otherwise noted.
 #
 class puppetdev (
   $user    = $::puppetdev::params::user,
   $group   = $::puppetdev::params::group,
 ) inherits puppetdev::params {
+
   include puppetdev::config
   include puppetdev::vim
   include puppetdev::vim_modules
   include puppetdev::vimrc
   include puppetdev::profile
+
 }
