15ed4cc0c58cb37df0431b9db638cc63860486d5
Jerald Sheets <questy@gmail.com>
2016-01-12 11:27:04 -0500

    Cleaned up tab artifacts and regenerated module

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 
 describe 'puppetdev', :type => 'class' do
 
+  it { should compile.with_all_deps }
+
+  it { is_expected.to contain_class('puppetdev::config') }
+  it { is_expected.to contain_class('puppetdev::vim') }
+  it { is_expected.to contain_class('puppetdev::vim_modules') }
+  it { is_expected.to contain_class('puppetdev::vimrc') }
+  it { is_expected.to contain_class('puppetdev::profile') }
+
   it "should contain the class" do
     should contain_class('puppetdev')
   end
+
 end
