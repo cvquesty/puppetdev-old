db2b5d3f7315877f77548a77157552d7be4d2211
Jerald Sheets <questy@gmail.com>
2016-01-14 14:48:08 -0500

    moved double-quoting to single quoting

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,16 +14,16 @@ class puppetdev::params {
 
   # The plugins to install into vim
   $plugins = {
-    "powerline" => "https://github.com/powerline/powerline.git",
-    "syntastic" => "https://github.com/scrooloose/syntastic.git",
-    "tabular" => "https://github.com/godlygeek/tabular.git",
-    "vim-airline" => "https://github.com/bling/vim-airline.git",
-    "vim-fugitive" => "https://github.com/tpope/vim-fugitive.git",
-    "vim-gitgutter" => "https://github.com/airblade/vim-gitgutter.git",
-    "vim-indent-guides" => "https://github.com/nathanaelkane/vim-indent-guides.git",
-    "vim-puppet" => "https://github.com/rodjek/vim-puppet.git",
-    "vim-trailing-whitespace" => "https://github.com/bronson/vim-trailing-whitespace.git",
-    "rainbow_parenthesis" => "https://github.com/kien/rainbow_parentheses.vim"
+    'powerline' => 'https://github.com/powerline/powerline.git',
+    'syntastic' => 'https://github.com/scrooloose/syntastic.git',
+    'tabular' => 'https://github.com/godlygeek/tabular.git',
+    'vim-airline' => 'https://github.com/bling/vim-airline.git',
+    'vim-fugitive' => 'https://github.com/tpope/vim-fugitive.git',
+    'vim-gitgutter' => 'https://github.com/airblade/vim-gitgutter.git',
+    'vim-indent-guides' => 'https://github.com/nathanaelkane/vim-indent-guides.git',
+    'vim-puppet' => 'https://github.com/rodjek/vim-puppet.git',
+    'vim-trailing-whitespace' => 'https://github.com/bronson/vim-trailing-whitespace.git',
+    'rainbow_parenthesis' => 'https://github.com/kien/rainbow_parentheses.vim'
   }
 
 }
