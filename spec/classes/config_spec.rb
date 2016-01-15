02193145809a159cf60cd34770213d1a3299561d
Jerald Sheets <questy@gmail.com>
2016-01-14 19:42:07 -0500

    renamed old config spec

--- spec/classes/config_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-  describe 'puppetdev::plugins', :type => 'class' do
-    let(:title) { 'plugins' }
-
-    it { is_expected.to contain_class('puppetdev::plugins') }
-
-  end
