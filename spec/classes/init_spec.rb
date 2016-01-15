5ea0d24252c44368811f564723abacb1c2e37d9b
Jerald Sheets <questy@gmail.com>
2016-01-14 19:46:38 -0500

    Stripping out all unnecessary tests to rewrite all of them.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,10 +4,6 @@ describe 'puppetdev', :type => 'class' do
 
   it { should compile.with_all_deps }
 
-  it { is_expected.to contain_class('puppetdev::prep') }
-  it { is_expected.to contain_class('puppetdev::plugins') }
-  it { is_expected.to contain_class('puppetdev::post') }
-
   it "should contain the class" do
     should contain_class('puppetdev')
   end
